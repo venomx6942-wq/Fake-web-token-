7029a55e9f323df986f42979080842f798c706ff
venomx6942-wq <venomx6942@gmail.com>
2026-05-17 07:59:12 +0530

    Create cheifip.php

---
+++ system/cheifip.php
@@ -0,0 +1,12 @@
+<?php
+function getUserIP() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
+$agung_ip_address = getUserIP();
+?>
