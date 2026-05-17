febba4c9551354832f30e6e76f71dae8e0dd4ff0
venomx6942-wq <venomx6942@gmail.com>
2026-05-17 14:41:51 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,23 @@
 <?php
-
 $token = "8603431466:AAGfzdIoWE7jEmziVEMhA5oNs2dEuhD1Az0";
-$data = [
-    'text' => $msg,
-    'chat_id' => '8586849798'
-];
+$chat_id = "8586849798";
 
+// Telegram message bhejne ka function
+function sendTelegram($token, $chat_id, $message) {
+    $url = "https://api.telegram.org/bot$token/sendMessage";
+    $data = [
+        'chat_id' => $chat_id,
+        'text' => $message,
+        'parse_mode' => 'HTML'
+    ];
+    $options = [
+        'http' => [
+            'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        ]
+    ];
+    $context = stream_context_create($options);
+    @file_get_contents($url, false, $context);
+}
 ?>
